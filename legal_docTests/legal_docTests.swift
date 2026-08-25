330250e1e07cc5f2139df349d1a5a35dcb28796c
mrjaiupadhyay <mrjaiupadhyay@gmail.com>
2026-08-25 23:02:49 +0530

    Create legal_docTests.swift

---
+++ legal_docTests/legal_docTests.swift
@@ -0,0 +1,17 @@
+//
+//  legal_docTests.swift
+//  legal_docTests
+//
+//  Created by jai upadhyay on 15/10/25.
+//
+
+import Testing
+@testable import legal_doc
+
+struct legal_docTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
