a75b82a9841d549da00822c9a8025d1476f55ae3
mrjaiupadhyay <mrjaiupadhyay@gmail.com>
2026-08-25 23:02:01 +0530

    Add files via upload

---
+++ legal_docUITests/legal_docUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  legal_docUITestsLaunchTests.swift
+//  legal_docUITests
+//
+//  Created by jai upadhyay on 15/10/25.
+//
+
+import XCTest
+
+final class legal_docUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
